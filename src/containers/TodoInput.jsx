c690631b3befea70d220585e752a1cf62376ede7
Ananda Umamil <arumamil@hotmail.com>
2018-09-15 09:03:38 +0700

    TodoInput.jsx: Add inline styles

--- src/containers/TodoInput.jsx
+++ src/containers/TodoInput.jsx
@@ -1,5 +1,30 @@
 import React, { Component, Fragment } from 'react';
 
+const style = {
+	form: {
+		width: '90%'
+	},
+	input: {
+		background: 'transparent',
+		border: '0px solid #20232a',
+		borderBottom: '1px solid #61dafb',
+		color: '#61dafb',
+		fontSize: '1.25em',
+		padding: '0.25em 1em',
+		textAlign: 'center',
+		width: '40%'
+	},
+	button: {
+		background: '#61dafb',
+		border: '3px solid #61dafb',
+		borderRadius: '5px',
+		color: '#000000',
+		fontSize: '1.25em',
+		margin: '0 0.2em',
+		padding: '0.25em 1em'
+	}
+};
+
 class TodoInput extends Component {
 	state = {
 		newItem: ''
@@ -17,9 +42,9 @@ class TodoInput extends Component {
 	render() {
 		return (
 			<Fragment>
-				<form onSubmit={this.handleSubmit}>
-					<input onChange={this.handleChange} value={this.state.newItem} type='text' />
-					<button>Add</button>
+				<form onSubmit={this.handleSubmit} style={style.form}>
+					<input onChange={this.handleChange} value={this.state.newItem} type='text' style={style.input} />
+					<button style={style.button}>Add</button>
 				</form>
 			</Fragment>
 		);
