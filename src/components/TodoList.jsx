104211cfc6b2f50cf34924058bb96581a751107d
Ananda Umamil <arumamil@hotmail.com>
2018-09-15 09:03:38 +0700

    TodoList.jsx: Add inline styles

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react';
 
+const style = {
+	item: {
+		color: '#61dafb',
+		fontSize: '1.5em',
+		marginBottom: '20%'
+	},
+	list: {
+		listStyle: 'none inside none',
+		padding: '0',
+		textAlign: 'left'
+	}
+};
+
 const TodoList = ({ todoItems }) => {
 	return (
 		<Fragment>
-			<ul>
-				{ todoItems.map(item => <li key={item.id}>{ item.text }</li>) }
+			<ul style={style.list}>
+				{ todoItems.map(item => <li key={item.id} style={style.item}>{ item.text }</li>) }
 			</ul>
 		</Fragment>
 	);
