b709903f87d424c542c5c0762d627e8abf3d9c23
Ananda Umamil <arumamil@hotmail.com>
2018-09-15 09:03:38 +0700

    App.jsx: Add layout and inline styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,35 @@ import React, { Component, Fragment } from 'react';
 import TodoList from './components/TodoList';
 import TodoInput from './containers/TodoInput';
 
+const style = {
+	content: {
+		alignItems: 'center',
+		display: 'flex',
+		backgroundColor: '#20232a',
+		bottom: '0',
+		justifyContent: 'top',
+		flexDirection: 'column',
+		fontFamily: 'Ubuntu, Roboto,sans-serif',
+		left: '0',
+		margin: 'auto',
+		maxHeight: '100%',
+		maxWidth: '100%',
+		overflow: 'auto',
+		position: 'absolute',
+		right: '0',
+		textAlign: 'center',
+		top: '0',
+	},
+	title: {
+		color: '#61dafb',
+		fontSize: '2em',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: '10%',
+		textAlign: 'center'
+	},
+};
+
 class App extends Component {
 	state = {
 		todoItems: []
@@ -19,11 +48,14 @@ class App extends Component {
 		}];
 		this.setState({ todoItems: newItem });
 	}
+	componentDidMount() {
+		document.title = "Todo App"
+	}
 	render() {
 		return (
 			<Fragment>
-				<div align='center'>
-					<h1>Todo App</h1>
+				<div style={style.content}>
+					<h1 style={style.title}>Todo App</h1>
 					<TodoList todoItems={this.state.todoItems} />
 					<TodoInput addNewItem={this.addNewItem} />
 				</div>
